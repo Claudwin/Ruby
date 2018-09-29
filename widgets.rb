9d46f73b13672c93c5948bd0119cfec4e49f24a6
Claudwin Fortune <claudwin.fortune@gmail.com>
2018-09-29 13:28:07 -0600

    Added spaces and concatnations

--- widgets.rb
+++ widgets.rb
@@ -1,1 +1,8 @@
-puts "Hello World"
+def ask(question)
+    print question + " "
+    gets
+end
+
+puts "Welcome to the widgets store"
+answer = ask("How many widgets are you ordering?")
+puts "You entered " + answer + " widgets"
