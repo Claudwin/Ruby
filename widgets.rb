008428cae72b95b222726a98132a5e3cfbedfadc
Claudwin Fortune <claudwin.fortune@gmail.com>
2018-10-02 12:07:07 -0600

    Practiced numbers and strings in Ruby

--- widgets.rb
+++ widgets.rb
@@ -1,8 +1,19 @@
 def ask(question)
     print question + " "
-    gets
+    gets.chomp
 end
-
+def price(quantity)
+    if quantity <= 49 
+        total = quantity * 10
+    elsif quantity >= 50 && <= 99
+        total = quantity * 9
+    else quantity >= 100
+        total = quantity * 8
+    end
+    return total
+end
 puts "Welcome to the widgets store"
 answer = ask("How many widgets are you ordering?")
-puts "You entered " + answer + " widgets"
+number = answer.to_i
+final = price(number)
+puts "The total cost for #{number} widgets is $#{number * 10}"
